2b46fe5b6819f67576bedfb173a077ce51495ff9
milemik <milemik68@gmail.com>
2024-03-09 14:20:52 +0100

    data_test - added test for TargetIdsList

--- data_test.go
+++ data_test.go
@@ -71,3 +71,35 @@ func TestBaseIdsList(t *testing.T) {
 		t.Errorf("Expect first id in List to be 1 and we got %s", result[0])
 	}
 }
+
+func TestTargetIdsListInit(t *testing.T) {
+	comp := AllCompanies{}
+
+	result := comp.TargetIdsList()
+	if len(result) != 0 {
+		t.Errorf("Expect that we have 0 base companies and we got %d", len(result))
+	}
+}
+
+func TestTargetIdsList(t *testing.T) {
+	comp := AllCompanies{All: []Company{
+		{Id: "1"},
+		{Id: "2"},
+		{Id: "3"},
+	}}
+
+	result := comp.TargetIdsList()
+	if len(result) != 3 {
+		t.Errorf("Expect that we have 3 base companies and we got %d", len(result))
+	}
+
+	if result[0] != "1" {
+		t.Errorf("Expect first id in List to be 1 and we got %s", result[0])
+	}
+	if result[1] != "2" {
+		t.Errorf("Expect first id in List to be 1 and we got %s", result[0])
+	}
+	if result[2] != "3" {
+		t.Errorf("Expect first id in List to be 1 and we got %s", result[0])
+	}
+}
