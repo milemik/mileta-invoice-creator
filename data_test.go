1097514b65eae7f896eb18cfdc7d57fb7a1a7259
milemik <milemik68@gmail.com>
2024-03-09 14:12:58 +0100

    Added test for adding base company

--- data_test.go
+++ data_test.go
@@ -6,10 +6,36 @@ func TestAddCompany(t *testing.T) {
 	comp := AllCompanies{}
 
 	compToAdd := Company{Id: "TestId"}
+	comp.AddCompany(compToAdd)
+
+	if len(comp.All) != 1 {
+		t.Errorf("Expected to have 1 company and got %d", len(comp.All))
+	}
+	// Try to add same company again
+	comp.AddCompany(compToAdd)
+
+	if len(comp.All) != 1 {
+		t.Errorf("Expected to have 1 company and got %d", len(comp.All))
+	}
+
+	if len(comp.Base) != 0 {
+		t.Errorf("Number of base companies should be 0 and we got %d", len(comp.Base))
+	}
+}
+
+func TestAddBaseCompany(t *testing.T) {
+	comp := AllCompanies{}
+
+	compToAdd := Company{Id: "TestTest", IsBaseCompany: true}
 
 	comp.AddCompany(compToAdd)
 
-	if len(comp.All) < 1 {
-		t.Errorf("Expected to have 1 company and got %d", len(comp.All))
+	if len(comp.All) != 0 {
+		t.Errorf("Expect that number of target companies is 0 and we got %d", len(comp.All))
 	}
+
+	if len(comp.Base) != 1 {
+		t.Errorf("Expect that number of Base companies is 1 and we got %d", len(comp.Base))
+	}
+
 }
